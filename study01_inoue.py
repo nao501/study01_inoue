11a89873fc0e08459fe5bb65a03aa0e96ea2a2c1
nao501 <79750877+nao501@users.noreply.github.com>
2021-03-01 22:52:29 +0900

    Add files via upload

--- study01_inoue.py
+++ study01_inoue.py
@@ -2,24 +2,24 @@ if  __name__ =='__main__':
     
     pass1=r'C:\Users\奈央也\Desktop\python (F)\test\character_name.csv'
     with open(pass1) as f:
-         character_name = list(f.readlines())
-         for a in character_name:
-             print(a.strip())
+         character_name = f.readlines()
+         character_deta =[ deta.strip() for deta in character_name ]
+         for  deta in character_name:
+             print(deta.strip())
         
 
 
 def search ():
     word = input('名前を入力してください：')
-    if word in character_name:
+    if word in character_deta:
         print('{}が見つかりました。'.format(word))
     else:
         print('{}が見つかりませんでした。'.format(word))
-        character_name.append(word)
+        character_deta.append(word)
 
 if  __name__ =='__main__':
     search()
-    pass1=r'C:\Users\奈央也\Desktop\python (F)\test\test.csv'
-    import csv
+    pass1=r'C:\Users\奈央也\Desktop\python (F)\test\character_name.csv'
     with open(pass1,mode='w') as f:
-         f.writelines("\n".join(str(character_name)))
+         f.writelines("\n".join(character_deta))
         
